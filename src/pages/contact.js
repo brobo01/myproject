cd8a5cbd3eb629a2500b8822cefb482ceac1cb9f
Ben Roberts <benroberts67@hotmail.co.uk>
2020-11-12 15:51:29 +0000

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Link} from "gatsby"
+import Header from "../components/header"
+import Container from "../components/container"
+
+
+export default function Contact(){
+  return(
+    <Container>
+      <Link to="/">Home</Link>
+      <Header headerText="Contact"/>
+      <p>Send us a message!</p>
+    </Container>
+  )
+}
